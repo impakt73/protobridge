2e2bfb52d8a896034e93fc78e16c0c3ff75673d7
Gregory Mitrano <gregory.mitrano@gmail.com>
2019-11-26 01:38:58 -0500

    Added New Memory Interface

--- inc/protobridge.h
+++ inc/protobridge.h
@@ -2,16 +2,17 @@
 #define PROTOBRIDGE_H
 
 #include <stdint.h>
+#include <stddef.h>
 
 typedef struct ProtoBridge_t* ProtoBridge;
 
 extern "C"
 {
 
-uint32_t CreateProtoBridge(ProtoBridge* phProtoBridge);
+uint32_t CreateProtoBridge(ProtoBridge* phProtoBridge, void* pMemory, size_t memorySize);
 void DestroyProtoBridge(ProtoBridge hProtoBridge);
 
-uint64_t UpdateProtoBridge(ProtoBridge hProtoBridge, uint64_t data);
+void UpdateProtoBridge(ProtoBridge hProtoBridge);
 
 }
 
--- src/protobridge.cpp
+++ src/protobridge.cpp
@@ -5,14 +5,19 @@
 struct ProtoBridgeContext
 {
     Vtop top;
+    void* pMemory;
+    size_t memorySize;
 };
 
-uint32_t CreateProtoBridge(ProtoBridge* phProtoBridge)
+uint32_t CreateProtoBridge(ProtoBridge* phProtoBridge, void* pMemory, size_t memorySize)
 {
     uint32_t result = 0xFFFFFFFF;
 
     ProtoBridgeContext* pContext = new ProtoBridgeContext();
     (*phProtoBridge) = reinterpret_cast<ProtoBridge>(pContext);
+
+    pContext->pMemory = pMemory;
+    pContext->memorySize = memorySize;
 
     pContext->top.i_clk = 0;
     pContext->top.i_rst = 1;
@@ -32,11 +37,32 @@ void DestroyProtoBridge(ProtoBridge hProtoBridge)
     delete pContext;
 }
 
-uint64_t UpdateProtoBridge(ProtoBridge hProtoBridge, uint64_t data)
+void UpdateProtoBridge(ProtoBridge hProtoBridge)
 {
     ProtoBridgeContext* pContext = reinterpret_cast<ProtoBridgeContext*>(hProtoBridge);
 
-    pContext->top.i_data = data;
+    // Handle memory reads
+    if (pContext->top.o_read_en && (pContext->pMemory != nullptr))
+    {
+        const uint64_t readAddr = pContext->top.o_read_addr;
+        if ((readAddr + sizeof(uint64_t)) <= pContext->memorySize)
+        {
+            uint64_t data;
+            memcpy(&data, reinterpret_cast<uint8_t*>(pContext->pMemory) + readAddr, sizeof(uint64_t));
+            pContext->top.i_data = data;
+        }
+    }
+
+    // Handle memory writes
+    if (pContext->top.o_write_en && (pContext->pMemory != nullptr))
+    {
+        const uint64_t writeAddr = pContext->top.o_write_addr;
+        if ((writeAddr + sizeof(uint64_t)) <= pContext->memorySize)
+        {
+            const uint64_t data = pContext->top.o_data;
+            memcpy(reinterpret_cast<uint8_t*>(pContext->pMemory) + writeAddr, &data, sizeof(uint64_t));
+        }
+    }
 
     pContext->top.i_clk = 1;
 
@@ -45,6 +71,4 @@ uint64_t UpdateProtoBridge(ProtoBridge hProtoBridge, uint64_t data)
     pContext->top.i_clk = 0;
 
     pContext->top.eval();
-
-    return pContext->top.o_data;
 }
